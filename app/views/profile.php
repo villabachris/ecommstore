9505df15f9483f2b675623d173e8959a41dc97a3
christopher villaba <villabachris@gmail.com>
2019-01-18 15:30:54 +0800

    update ecommstore

--- app/views/profile.php
+++ app/views/profile.php
@@ -79,7 +79,7 @@
 										 		 	
                                           		<td><?php echo $transaction['transaction_code']?></td>
                                           		<td><?php echo $transaction['purchase_date'] ?></td>
-                                          		<td class="<?php if($transaction['status'] == "pending"){echo "text-warning";}else{echo "text-success";} ?>"><?php echo $transaction['status'] ?></td>
+                                          		<td class="<?php if($transaction['status'] == "pending"){echo "text-warning";}elseif($transaction['status']=="completed"){echo "text-success";}"text-danger"{} ?>"><?php echo $transaction['status'] ?></td>
                                           		<td><?php echo $transaction['payment_modes'] ?></td>
                                           	</tr>
                                         <?php  }?>
